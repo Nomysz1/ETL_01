18a22128fa370dad38cf346ae968d5f4bb38a58d
Simon <simonarch12@gmail.com>
2026-02-01 21:14:53 +0100

    Implementation of CSV-to-Parquet conversion

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import os.path
+
 import duckdb
 import kagglehub
 
 #CONSTANTS
-AV_THREADS = 2
-KAGGLE_LINK = 'https://www.kaggle.com/datasets/mhassansaboor/alibaba-stock-dataset-2025'
+AV_THREADS:int = 2
+KAGGLE_LINK:str = 'mhassansaboor/alibaba-stock-dataset-2025'
+
+source_path = ''
 
 def download_csv():
-    print("Downloading CSV...")
+    global source_path
     try:
-        kagglehub.login()
+        print("Downloading CSV...")
+        source_path = kagglehub.dataset_download(KAGGLE_LINK)
+        print(f"Download complete. {source_path}")
     except Exception as e:
         print(e)
 
@@ -21,4 +27,16 @@ def create_db(db_name: str):
         return db
 
 if __name__ == '__main__':
-    connection = create_db('dwh.db')
+    try:
+        download_csv()
+        connection = create_db('dwh.db')
+        f_path:str = ''
+        with os.scandir(source_path) as entries:
+            for entry in entries:
+                if entry.is_file() and entry.name.endswith('.csv'):
+                    f_path = os.path.join(source_path, entry.name)
+                    connection.read_csv(f_path).write_parquet(entry.name[:-4] + '.parquet')
+                    connection.read_parquet(entry.name[:-4] + '.parquet')
+
+    except Exception as e:
+        print(e)
